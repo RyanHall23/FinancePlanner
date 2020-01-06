19decbf991200bb0bea427753247dfcf31cc9f58
Ryan Hall <rj23hall@gmail.com>
2020-01-06 15:51:33 +0000

    v0.14 - Added grid to create expense reports

--- FinancePlanner/Navigation Pages/SavingsPage.xaml.cs
+++ FinancePlanner/Navigation Pages/SavingsPage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace FinancePlanner.Navigation_Pages
@@ -8,10 +10,33 @@ namespace FinancePlanner.Navigation_Pages
     /// </summary>
     public partial class SavingsPage : Page
     {
+        COverview ov = new COverview();
+        CSavings svgs = new CSavings();
+
+        decimal decExpensesMo;
+
         public SavingsPage()
         {
             InitializeComponent();
             lblDateTime.Content = DateTime.Now.ToShortDateString(); // Sets the Date label to the current Date
+            txtSavingsMonthlyAmnt.Text = svgs.GetMonthSavings().ToString();
+            lblSavingsMonthAmnt.Content = svgs.GetMonthSavings();
+            lblSavingsYearlyAmnt.Content = svgs.GetYearlyExpenses();
+        }
+
+        private void BtnCnfmMonthlyExpenses_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                decimal.TryParse(txtSavingsMonthlyAmnt.Text, out decExpensesMo);
+                svgs.SetMonthlyAndYearlySavings(decExpensesMo);
+                ov.SetExpense(decExpensesMo);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+
         }
     }
 }
